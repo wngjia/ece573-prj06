125370150d06b3143967907274853adad420d09c
Jia <Jia>
2024-05-16 05:33:44 +0000

    add chaos-mesh dns image

--- reset_cluster.sh
+++ reset_cluster.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+docker pull ghcr.io/chaos-mesh/chaos-coredns:v0.2.6
 docker pull ghcr.io/chaos-mesh/chaos-mesh:v2.6.2
 docker pull ghcr.io/chaos-mesh/chaos-daemon:v2.6.2
 docker pull ghcr.io/chaos-mesh/chaos-dashboard:v2.6.2
@@ -7,6 +8,7 @@ docker pull confluentinc/cp-zookeeper:7.3.5
 docker pull confluentinc/cp-kafka:7.3.5
 kind delete cluster
 kind create cluster --config cluster.yml
+kind load docker-image ghcr.io/chaos-mesh/chaos-coredns:v0.2.6
 kind load docker-image ghcr.io/chaos-mesh/chaos-mesh:v2.6.2
 kind load docker-image ghcr.io/chaos-mesh/chaos-daemon:v2.6.2
 kind load docker-image ghcr.io/chaos-mesh/chaos-dashboard:v2.6.2
