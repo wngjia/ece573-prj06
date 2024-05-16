0e621fd1083ce07307d3e6a85921fc99fee2a344
Jia <Jia>
2024-05-16 00:27:56 +0000

    update kind and k8s version

--- setup_vm.sh
+++ setup_vm.sh
@@ -13,14 +13,14 @@ curl -OL https://go.dev/dl/go1.21.0.linux-amd64.tar.gz
 tar -C /usr/local -xvf go1.21.0.linux-amd64.tar.gz
 rm go1.21.0.linux-amd64.tar.gz
 
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO "https://dl.k8s.io/release/v1.30.0/bin/linux/amd64/kubectl"
 install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 rm kubectl
 
 echo 'export PATH=$PATH:/usr/local/go/bin:/home/ubuntu/go/bin' >> /home/ubuntu/.bashrc
 
 sudo -i -u ubuntu bash << EOF
-/usr/local/go/bin/go install sigs.k8s.io/kind@v0.20.0
+/usr/local/go/bin/go install sigs.k8s.io/kind@v0.23.0
 EOF
 
 su - ubuntu
